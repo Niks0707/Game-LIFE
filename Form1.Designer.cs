e3749df7c8c2c1b28f8095935cf7d4bc8759638c
Niks0707 <niks.borisov@gmail.com>
2016-10-25 22:26:49 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.repickToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rabbitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gLAYDERToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(779, 347);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.repickToolStripMenuItem,
+            this.nextToolStripMenuItem,
+            this.rabbitToolStripMenuItem,
+            this.gLAYDERToolStripMenuItem,
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(779, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // repickToolStripMenuItem
+            // 
+            this.repickToolStripMenuItem.Name = "repickToolStripMenuItem";
+            this.repickToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.repickToolStripMenuItem.Text = "repick";
+            this.repickToolStripMenuItem.Click += new System.EventHandler(this.repickToolStripMenuItem_Click);
+            // 
+            // nextToolStripMenuItem
+            // 
+            this.nextToolStripMenuItem.Name = "nextToolStripMenuItem";
+            this.nextToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.nextToolStripMenuItem.Text = "Next";
+            this.nextToolStripMenuItem.Click += new System.EventHandler(this.nextToolStripMenuItem_Click);
+            // 
+            // rabbitToolStripMenuItem
+            // 
+            this.rabbitToolStripMenuItem.Name = "rabbitToolStripMenuItem";
+            this.rabbitToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.rabbitToolStripMenuItem.Text = "Rabbit";
+            this.rabbitToolStripMenuItem.Click += new System.EventHandler(this.rabbitToolStripMenuItem_Click);
+            // 
+            // gLAYDERToolStripMenuItem
+            // 
+            this.gLAYDERToolStripMenuItem.Name = "gLAYDERToolStripMenuItem";
+            this.gLAYDERToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.gLAYDERToolStripMenuItem.Text = "GLAYDER";
+            this.gLAYDERToolStripMenuItem.Click += new System.EventHandler(this.gLAYDERToolStripMenuItem_Click);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDown1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(779, 376);
+            this.splitContainer1.SplitterDistance = 25;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.numericUpDown1.Location = new System.Drawing.Point(0, 0);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 400);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem repickToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gLAYDERToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rabbitToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Life
+{
+    public partial class Form1 : Form
+    {
+        Life l;
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.Value = timer1.Interval;
+            //Camalon.Drawing.Shapes.Pie pie = new Camalon.Drawing.Shapes.Pie();
+            //pie.Size=new Size(100,100);            
+            //Graphics g=Graphics.FromHdc(//.FromHwndInternal(this.Handle);
+            //g.DrawPolygon(new Pen(Brushes.Brown),new Point[]{
+            //    new Point(20,20),
+            //    new Point(30,20),
+            //    new Point(30,30),
+            //    new Point(40,20),
+            //    new Point(50,20),
+            //});
+            //g.DrawPie(new Pen(Brushes.Black), new Rectangle(10, 10, 100, 100), 1, 30);
+            //pie.Paint();
+        }
+
+        public void New()
+        {
+            l = new Life(new Point(30, 30),250);
+        }
+
+        private void show()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.ColumnCount = l.Size().Y;
+            dataGridView1.RowCount = l.Size().X;
+            for (int i = 0; i < l.Size().Y; i++)
+            {
+                dataGridView1.Columns[i].Width = 25;
+                for (int j = 0; j < l.Size().X; j++)
+                    if (l.a[j][i] == 1)
+                    {
+                        dataGridView1[i, j].Style.BackColor = Color.Red;
+                    }
+            }
+            
+        }
+
+        private void repickToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            New();
+            show();
+        }
+
+        private void nextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            l.Next();
+            show();
+        }
+
+        private void gLAYDERToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            l = new Life(Samples.Glayder);
+            show();
+        }
+
+        private void rabbitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            l = new Life(Samples.Rabbit);
+            show();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //if (toolStripMenuItem1.CheckState==CheckState.Checked)
+            l.Next();
+            //if (toolStripMenuItem3.CheckState==CheckState.Checked)
+            //    for (int i = 0; i < 10; i++)
+            //        l.Next();
+            show();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = (int)numericUpDown1.Value;
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+
+
+
+    class Life
+    {
+        public int[][] a;
+        Point size;
+        Random rand = new Random(unchecked((int)(DateTime.Now.Ticks)));
+        struct neighbors
+        {
+            public int xleft, ytop, xright, ydown;
+        }
+
+        public Life()
+        {
+            size.X = rand.Next(5, 30);
+            size.Y = rand.Next(5, 30);
+            a = new int[size.X][];
+            for (int i = 0; i < size.X; i++)
+            {
+                a[i] = new int[size.Y];
+                for (int j = 0; j < size.Y; j++)
+                    if (rand.Next(1, 50) == 1)
+                        a[i][j] = 1;
+            }
+        }
+
+        public Life(Point Size)
+        {
+            size = Size;
+            a = new int[size.X][];
+            for (int i = 0; i < size.X; i++)
+            {
+                a[i] = new int[size.Y];
+                for (int j = 0; j < size.Y; j++)
+                    if (rand.Next(1, 50) == 1)
+                        a[i][j] = 1;
+            }
+        }
+
+        public Life(Point Size, byte frequent)
+        {
+            size = Size;
+            a = new int[size.X][];
+            for (int i = 0; i < size.X; i++)
+            {
+                a[i] = new int[size.Y];
+                for (int j = 0; j < size.Y; j++)
+                    if (rand.Next(1, 256 - frequent) == 1)
+                        a[i][j] = 1;
+            }
+        }
+
+        public Life(int[][] m)
+        {
+            size.X = m.Length;
+            size.Y = m[0].Length;
+            a = m;
+        }
+
+        public Point Size()
+        {
+            return size;
+        }
+
+        public int[][] life()
+        {
+            return a;
+        }
+
+        private void Neighbors(out neighbors neighbs, int I, int J)
+        {
+            neighbs = new neighbors();
+            neighbs.xleft = I - 1;
+            neighbs.xright = I + 1;
+            neighbs.ytop = J - 1;
+            neighbs.ydown = J + 1;
+            if (I - 1 < 0)
+                neighbs.xleft = size.X - 1;
+            if (I + 1 > size.X - 1)
+                neighbs.xright = 0;
+            if (J - 1 < 0)
+                neighbs.ytop = size.Y - 1;
+            if (J + 1 > size.Y - 1)
+                neighbs.ydown = 0;
+        }
+
+        public void Next()
+        {
+            int[][] temp = a;
+            int count;
+            for (int i = 0; i < size.X; i++)
+                for (int j = 0; j < size.Y; j++)
+                {
+                    neighbors neighbors;
+                    Neighbors(out neighbors, i, j);
+                    count = 0;
+                    if (a[neighbors.xleft][neighbors.ytop] > 0)
+                        count++;
+                    if (a[i][neighbors.ytop] > 0)
+                        count++;
+                    if (a[neighbors.xright][neighbors.ytop] > 0)
+                        count++;
+                    if (a[neighbors.xleft][j] > 0)
+                        count++;
+                    if (a[neighbors.xright][j] > 0)
+                        count++;
+                    if (a[neighbors.xleft][neighbors.ydown] > 0)
+                        count++;
+                    if (a[i][neighbors.ydown] > 0)
+                        count++;
+                    if (a[neighbors.xright][neighbors.ydown] > 0)
+                        count++;
+                    if (count < 2 || count > 3)
+                        temp[i][j] = 0;
+                    if (count == 3 && a[i][j] == 0)
+                        temp[i][j] = 1;
+                }
+            a = temp;
+        }
+    }
+
+    static class Samples
+    {
+        public static readonly int[][] Glayder = new int[][]
+            {
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+        
+        public static readonly int[][] Rabbit = new int[][]
+            {        
+                new int[]{0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,1,1,1,1,0,0,0,1,1,1,1,1,1,0,0,0,0},
+                new int[]{0,0,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,0,0,0},
+                new int[]{0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0,0},
+                new int[]{1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+                new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+                new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                new int[]{0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                new int[]{0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                new int[]{0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                new int[]{0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                new int[]{0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                new int[]{0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                new int[]{0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+                new int[]{0,0,0,0,0,0,1,1,1,1,1,0,1,1,1,1,1,0,0,0},
+                new int[]{0,0,0,0,0,1,1,1,1,0,0,1,1,1,1,1,0,0,0,0}};
+
+    }
+
+    /*
+    class Rectangles
+    {
+        Rectangle[,] rect;
+        int width = 0;
+        int height = 0;
+        int i = 0;
+        int j = 0;
+        int N;
+        int M;
+        int zoom;
+        int k = 0;
+        int l = 0;
+
+        public Rectangles(int n, int m)
+        {
+            N = n;
+            M = m;
+            rect = new Rectangle[n, m];
+        }
+
+        public void DrawRectangle(int w, int h, Color color, int beginX, int beginY, int size)
+        {
+            width = w / M;
+            height = h / N;
+            if (height > width)
+                zoom = width;
+            else
+                zoom = height;
+            if (j == M)
+            {
+                i++;
+                if (i == N)
+                    i = 0;
+                j = 0;
+                l = 0;
+            }
+            if (i == 0 && j == 0)
+            {
+                k = 0;
+                l = 0;
+            }
+            for (int I = size; I < N; I += 5)
+                if (i == I && j == 0)
+                    k += 3;
+            for (int J = 0; J < M; J++)
+                if (j == J * 5 + size)
+                {
+                    l += 3;
+                }
+            rect[i, j] = new Rectangle(beginX + j * zoom + 10 + l,
+                beginY + i * zoom + 10 + k, zoom - 3, zoom - 3);
+        }
+
+        public Rectangle Rectan(int I, int J)
+        {
+            return rect[I, J];
+        }
+    }
+     */
+}
+
